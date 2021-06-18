bb2073e3c8f2e0b991969c11a6086a5b319d3b4f
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-18 17:30:41 +0700

    cleaned up Card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 import { PageWrapper, Container } from './components/Layout'
 import { Header } from 'app/components/Header'
-import { Card } from 'app/components/Ticket/Card'
+import { Lane } from 'app/components/Ticket/Lane'
 
 export const App = (): JSX.Element => (
   <PageWrapper>
     <Container>
       <Header />
-      <div className="w-1/4">
-        <Card />
-      </div>
+      <Lane />
     </Container>
   </PageWrapper>
 )
