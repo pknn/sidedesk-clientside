337edb21d3e66b24df83eb501e711a3a8161d553
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-18 19:35:30 +0700

    edited fake date to use the same day, prevent snapshot error

--- src/helpers/mockTicket.ts
+++ src/helpers/mockTicket.ts
@@ -9,7 +9,20 @@ export const ticketStatusOptions: TicketStatus[] = [
   TicketStatus.Rejected,
 ]
 
-const getFakeTicket = (status?: TicketStatus): Ticket => {
+type DateIndex = 0 | 1 | 2
+
+const dates: Date[] = [
+  new Date(1623862800),
+  new Date(1623949200),
+  new Date(1624035600),
+]
+
+const getFakeDate = (dateIndex: DateIndex): Date => dates[dateIndex]
+
+const getFakeTicket = (
+  status?: TicketStatus,
+  dateIndex?: DateIndex,
+): Ticket => {
   const firstName = faker.name.firstName()
   const lastName = faker.name.lastName()
   const fullName = `${firstName} ${lastName}`
@@ -20,8 +33,8 @@ const getFakeTicket = (status?: TicketStatus): Ticket => {
     reporterName: fullName,
     reporterEmail: faker.internet.email(firstName, lastName),
     status: status || faker.random.arrayElement(ticketStatusOptions),
-    createdAt: faker.date.future(),
-    updatedAt: faker.date.future(),
+    createdAt: getFakeDate(dateIndex || 0),
+    updatedAt: getFakeDate(dateIndex || 0),
   }
 }
 
@@ -34,7 +47,9 @@ export const getMockTickets = (n: number, status?: TicketStatus): Ticket[] => {
   faker.seed(96421)
   const tickets: Ticket[] = []
   for (let i = 0; i < n; i += 1) {
-    tickets.push(getFakeTicket(status))
+    const fa = i % 3 == 0
+    const fb = i % 3 == 1
+    tickets.push(getFakeTicket(status, fa ? 0 : fb ? 1 : 2))
   }
   return tickets
 }
