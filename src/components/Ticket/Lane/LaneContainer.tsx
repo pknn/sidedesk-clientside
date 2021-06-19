5229d61ed3d5b380036f49a9d34ce73587735bfe
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-19 11:55:11 +0700

    structured Lane component

---
+++ src/components/Ticket/Lane/LaneContainer.tsx
@@ -0,0 +1,53 @@
+import { Children } from 'app/types/Primitive'
+import React from 'react'
+import { DroppableProvided, DroppableStateSnapshot } from 'react-beautiful-dnd'
+import { Basis } from './LaneComponent.styled'
+
+interface PrimitiveProps {
+  children: Children
+}
+
+interface DroppableProps {
+  droppableProvided: DroppableProvided
+  droppableStateSnapshot: DroppableStateSnapshot
+}
+
+type ComponentProps = PrimitiveProps & DroppableProps
+
+const getLaneClassNames = (
+  isDraggingOver: boolean,
+  isDraggingFrom: boolean,
+) => {
+  let color = 'bg-gray-50'
+  if (isDraggingOver) {
+    if (isDraggingFrom) {
+      color = 'bg-blue-100'
+    } else {
+      color = 'bg-green-100'
+    }
+  } else if (isDraggingFrom) {
+    color = 'bg-red-100'
+  }
+  return [
+    'rounded shadow-sm h-full relative transition-colors duration-150',
+    color,
+  ].join(' ')
+}
+
+export const LaneContainer = ({
+  children,
+  droppableProvided,
+  droppableStateSnapshot,
+}: ComponentProps): JSX.Element => (
+  <Basis>
+    <div
+      ref={droppableProvided.innerRef}
+      className={getLaneClassNames(
+        droppableStateSnapshot.isDraggingOver,
+        !!droppableStateSnapshot.draggingFromThisWith,
+      )}
+    >
+      {children}
+    </div>
+  </Basis>
+)
---
+++ src/components/Ticket/Lane/LaneContent.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { Card } from 'app/components/Ticket/Card'
+import { Ticket } from 'app/types/Ticket'
+import { DroppableProvided } from 'react-beautiful-dnd'
+
+interface DataProps {
+  tickets: Ticket[]
+}
+
+interface DroppableProps {
+  droppableProvided: DroppableProvided
+}
+
+type ComponentProps = DataProps & DroppableProps
+
+export const LaneContent = ({ tickets, droppableProvided }: ComponentProps) => (
+  <div className="p-2 pt-0">
+    {tickets.map((ticket, index) => (
+      <Card
+        key={ticket.id}
+        index={index}
+        id={ticket.id}
+        title={ticket.title}
+        reporterName={ticket.reporterName}
+      />
+    ))}
+    {droppableProvided.placeholder}
+  </div>
+)
