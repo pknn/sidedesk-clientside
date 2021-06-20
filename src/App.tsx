75a6f068e238f53233ecddae194268a7973686d1
Pakanon Pantisawat <pakanon.pantisawat@gmail.com>
2021-06-20 22:56:59 +0700

    added EditTicketContent component inner

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { PageWrapper, Container } from './components/Layout'
 import { Header } from 'app/components/Header'
 import { Board } from 'app/components/Ticket/Board'
+import { EditTicket } from './components/Modals/EditTicket'
 
 export const App = (): JSX.Element => (
   <PageWrapper>
+    <EditTicket />
     <Container>
       <Header />
       <Board />
--- src/components/Modals/EditTicket/EditTicketContent.tsx
+++ src/components/Modals/EditTicket/EditTicketContent.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const EditTicketContent = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-semibold">Hi</h1>
+      <input
+        type="text"
+        className="p-2 text-xl font-semibold outline-none rounded hover:bg-gray-200 focus:bg-white focus:ring-2 focus:ring-blue-500"
+        placeholder="Title"
+      />
     </div>
   )
 }
